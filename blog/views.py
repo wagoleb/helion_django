ab73fc66dfe63b7453fb46a9d2274d0d34789781
bonowg <bonowg@gmail.com>
2017-09-03 21:11:18 +0200

    widok pojedynczego posta

--- blog/views.py
+++ blog/views.py
@@ -6,8 +6,16 @@ from .models import Post
 
 
 def postsList(request):
-
     posts = Post.published.all()
-
     return render(request, 'blog/post/list.html', {'posts': posts})
 
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        slug = post,
+        publish__year = year,
+        publish__month = month,
+        publish__day = day
+    )
+
+    return render(request, 'blog/post/post_detail.html', {'post': post})
