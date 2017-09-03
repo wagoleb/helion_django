95bcc173bf034de5bce804c38bd5581d152aa680
bonowg <bonowg@gmail.com>
2017-09-03 19:34:01 +0200

    dodanie widoku

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
 
 # Create your views here.
+
+
+def postsList(request):
+
+    posts = Post.object.all()
+
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
