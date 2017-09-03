e632468d0a7906e5deaf090293f1a21cafd7407c
bonowg <bonowg@gmail.com>
2017-09-03 21:16:34 +0200

    dodane urele

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,7 @@ def postsList(request):
     posts = Post.published.all()
     return render(request, 'blog/post/list.html', {'posts': posts})
 
-def post_detail(request, year, month, day, post):
+def postDetail(request, year, month, day, post):
     post = get_object_or_404(
         Post,
         slug = post,
